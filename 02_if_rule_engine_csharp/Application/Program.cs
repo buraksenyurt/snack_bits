b5b08f4167fd33aa0aa4c5356b7b44502239f18d
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-10 20:09:23 +0300

    Implemented rule engine instead of if :P

--- 02_if_rule_engine_csharp/Application/Program.cs
+++ 02_if_rule_engine_csharp/Application/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Domain;
+using RulesEngine.Models;
 
 public class Program
 {
@@ -56,32 +57,148 @@ public class Program
                 }
             };
 
+        #region Initial State 
+
+        // foreach (var order in orders)
+        // {
+        //     if (order.Quantity < monitor.StockLevel)
+        //     {
+        //         int discountRate;
+        //         switch (order.CustomerType)
+        //         {
+        //             case CustomerType.AuthorizedDealer:
+        //                 discountRate = 2;
+        //                 break;
+        //             case CustomerType.Distributor:
+        //                 discountRate = 5;
+        //                 break;
+        //             case CustomerType.Individual:
+        //                 discountRate = 3;
+        //                 break;
+        //             case CustomerType.GroupFirm:
+        //                 discountRate = 18;
+        //                 break;
+        //             default:
+        //                 discountRate = 0;
+        //                 break;
+        //         }
+        //         Console.WriteLine($"İndirim tutarı % {discountRate}");
+        //     }
+        // }
+
+        #endregion
+
+        #region Rule Engine State
+
+        var workflows = new List<Workflow>() {
+                new Workflow
+                    {
+                        WorkflowName = "Discounting Rules",
+                        Rules = new List<Rule>
+                        {
+                            new Rule
+                            {
+                                RuleName="Individual Order",
+                                Expression = "input2.Quantity < input1.StockLevel && input2.CustomerType == CustomerType.Individual",
+                                RuleExpressionType = RuleExpressionType.LambdaExpression,
+                                Actions = new RuleActions
+                                {
+                                    OnSuccess= new ActionInfo
+                                    {
+                                        Name = "OutputExpression",
+                                        Context = new Dictionary<string, object>()
+                                        {
+                                            { "Expression",3 }
+                                        }
+                                    }
+                                }
+                            },
+                            new Rule
+                            {
+                                RuleName="Distributor Order",
+                                Expression = "input2.Quantity < input1.StockLevel && input2.CustomerType == CustomerType.Distributor",
+                                RuleExpressionType = RuleExpressionType.LambdaExpression,
+                                Actions = new RuleActions
+                                {
+                                    OnSuccess= new ActionInfo
+                                    {
+                                        Name = "OutputExpression",
+                                        Context = new Dictionary<string, object>()
+                                        {
+                                            { "Expression",5 }
+                                        }
+                                    }
+                                }
+                            },
+                            new Rule
+                            {
+                                RuleName="Authorized Dealer",
+                                Expression = "input2.Quantity < input1.StockLevel && input2.CustomerType == CustomerType.AuthorizedDealer",
+                                RuleExpressionType = RuleExpressionType.LambdaExpression,
+                                Actions = new RuleActions
+                                {
+                                    OnSuccess= new ActionInfo
+                                    {
+                                        Name = "OutputExpression",
+                                        Context = new Dictionary<string, object>()
+                                        {
+                                            { "Expression",2 }
+                                        }
+                                    }
+                                }
+                            },
+                            new Rule
+                            {
+                                RuleName="Group Firm",
+                                Expression = "input2.Quantity < input1.StockLevel && input2.CustomerType == CustomerType.GroupFirm",
+                                RuleExpressionType = RuleExpressionType.LambdaExpression,
+                                Actions = new RuleActions
+                                {
+                                    OnSuccess= new ActionInfo
+                                    {
+                                        Name = "OutputExpression",
+                                        Context = new Dictionary<string, object>()
+                                        {
+                                            { "Expression",18 }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                }
+            };
+
+        var ruleEngine = new RulesEngine.RulesEngine(workflows.ToArray());
         foreach (var order in orders)
         {
-            if (order.Quantity < monitor.StockLevel)
+            var inputs = new dynamic[]
+                        {
+                                monitor,
+                                order,
+                                order.CustomerType
+                        };
+            var results = ruleEngine.ExecuteAllRulesAsync("Discounting Rules", inputs).Result;
+            foreach (var ruleResult in results)
             {
-                int discountRate;
-                switch (order.CustomerType)
+                if (ruleResult.ActionResult != null && ruleResult.IsSuccess)
                 {
-                    case CustomerType.AuthorizedDealer:
-                        discountRate = 2;
-                        break;
-                    case CustomerType.Distributor:
-                        discountRate = 5;
-                        break;
-                    case CustomerType.Individual:
-                        discountRate = 3;
-                        break;
-                    case CustomerType.GroupFirm:
-                        discountRate = 18;
-                        break;
-                    default:
-                        discountRate = 0;
-                        break;
+                    Console.WriteLine($"'{ruleResult.Rule.RuleName}' için beklenen indirim oranı '% {ruleResult.ActionResult.Output}'");
                 }
-                Console.WriteLine($"İndirim tutarı % {discountRate}");
             }
+
+            //results.OnSuccess((eventName) =>
+            //{
+            //    Console.WriteLine($"'{eventName}' isimli olay tetiklendi. Sipariş durumu");
+            //});
+
+            //results.OnFail(() =>
+            //{
+            //    Console.WriteLine("Sipariş verilemez");
+            //});
         }
+
+        #endregion
+
     }
 }
 
