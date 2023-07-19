9e673da634b2ea29d96f27eea9b03ccad57b9e6e
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-19 10:31:59 +0300

    Applied first soluitıon.

---
+++ 04_constants_to_enum_csharp/Application/Output.cs
@@ -0,0 +1,7 @@
+public enum ProcessTypeEnum
+{
+	Student = 1001,
+	Beginner = 1005,
+	Premium = 7007,
+	Employee = 9042,
+}
--- 04_constants_to_enum_csharp/Application/Program.cs
+++ 04_constants_to_enum_csharp/Application/Program.cs
@@ -1,8 +1,33 @@
-﻿
+﻿using System.Reflection;
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using Domain;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var fields = typeof(ProcessType)
+            .GetFields(BindingFlags.Public | BindingFlags.Static)
+            .Where(fi => fi.IsLiteral && !fi.IsInitOnly);
+
+        var builder = new StringBuilder();
+        builder.AppendLine($"public enum {nameof(ProcessType)}Enum");
+        builder.AppendLine("{");
+        foreach (var field in fields)
+        {
+            builder.AppendLine($"\t{field.Name} = {typeof(ProcessType).GetField(field.Name).GetValue(null)},");
+        }
+        builder.AppendLine("}");
+
+        File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "Output.cs"), builder.ToString());
+    }
+}
+
 namespace Domain
 {
-
-    public static class CustomerType
+    public static class ProcessType
     {
         public const int Student = 1001;
         public const int Beginner = 1005;
@@ -10,3 +35,6 @@ namespace Domain
         public const int Employee = 9042;
     }
 }
+
+
+
