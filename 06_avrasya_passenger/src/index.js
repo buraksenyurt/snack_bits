259a256550afab15ffa21c4cacb59dd460288751
buraksenyurt <burakselimsenyurt@gmail.com>
2023-10-30 20:55:57 +0300

    Resolved the server problem.

--- 06_avrasya_passenger/src/index.js
+++ 06_avrasya_passenger/src/index.js
@@ -1,12 +1,27 @@
 import avrasya from "avrasya";
 
-var server = new avrasya();
+const server = new avrasya.default();
 
-server.router.get("/", (ctx) => {
+server.router.get("/product/{id}", (ctx) => {
+    console.log(ctx.req);
     ctx.send("Wellcome to my real world");
 });
 
-server.middleware.add((ctx) => {
+server.router.post("/product", (ctx) => {
+    console.log(ctx.body);
+    ctx.send("Post request");
+})
+
+server.router.delete("/product/id",(ctx)=>{
+    ctx.send("Delete request");
+})
+
+server.router.put("/product",(ctx)=>{
+    console.log(ctx.body);
+    ctx.send("Put request");
+})
+
+server.use((ctx) => {
     console.log("middleware");
     console.log(ctx.req.url + " " + ctx.req.method);
 })
