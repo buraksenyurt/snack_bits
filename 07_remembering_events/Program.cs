513751a80bb1fb97f328fc07e1a2927ebe96e311
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2023-12-13 08:17:42 +0000

    Added location type.

--- 07_remembering_events/Program.cs
+++ 07_remembering_events/Program.cs
@@ -8,6 +8,74 @@ static class Program
     }
 }
 
+struct WarningConfiguration
+{
+    public double HeatAlert;
+    public double HumidityAlert;
+    public WarningConfiguration(double heatAlert, double humidityAlert)
+    {
+        HeatAlert=heatAlert;
+        HumidityAlert=humidityAlert;
+    }
+}
+
+public enum WeatherType
+{
+    Sunny,
+    Rainy,
+    Cloudy,
+    Snowy,
+    Stormy
+}
+
+class Location
+{
+    private readonly WarningConfiguration _warningConfiguration;
+    private double heat;
+    private double humidity;
+    public event OverloadEventHandler HeatOverloaded;
+    public event OverloadEventHandler HumidityOverloaded;
+    public string Title { get; set; }
+    public WeatherType WeatherType { get; set; }
+    public Location(WarningConfiguration warningConfiguration, string title, WeatherType weatherType)
+    {
+        _warningConfiguration = warningConfiguration;
+        Title = title;
+        WeatherType = weatherType;
+    }
+    public double Heat
+    {
+        get
+        {
+            return heat;
+        }
+        set
+        {
+            if (value > _warningConfiguration.HeatAlert
+                && HeatOverloaded != null)
+            {
+                HeatOverloaded(this, new OverloadEventArgs { Value = value, AlertType = AlertType.Heat });
+            }
+            heat = value;
+        }
+    }
+    public double Humidity
+    {
+        get
+        {
+            return humidity;
+        }
+        set
+        {
+            if (value > _warningConfiguration.HumidityAlert
+                && HumidityOverloaded != null)
+            {
+                HumidityOverloaded(this, new OverloadEventArgs { Value = value, AlertType = AlertType.Humidity });
+            }
+            humidity = value;
+        }
+    }
+}
 
 public enum AlertType
 {
