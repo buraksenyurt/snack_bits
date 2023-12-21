367d1c4e639b9e44c2ab24fa14a5485c5a8a3a87
buraksenyurt <burakselimsenyurt@gmail.com>
2023-12-21 20:43:05 +0300

    Applied a test with mock object.

--- 09_mocking_tests/ManagerTests.cs
+++ 09_mocking_tests/ManagerTests.cs
@@ -1,10 +1,22 @@
+using KanbanWorld;
+using Moq;
+
 namespace _09_mocking_tests;
 
 public class ManagerTests
 {
     [Fact]
-    public void Test1()
+    public void GetCount_Returns_3_Test()
     {
+        var mockFileLoader = new Mock<IFileLoader>();
+        mockFileLoader.Setup(m => m.LoadLines(It.IsAny<string>())).Returns(new List<string>{
+           "1|Haftasonu unit test konusuna bak|50|M|false",
+           "2|Denizler Altında Yirmi Bin Fersah kitabının özetini çıkart|90|XL|false",
+           "3|5 Km yürüyüş yap|100|S|true",
+        });
 
+        var manager = new Manager(mockFileLoader.Object);
+        var actual = manager.GetCount();
+        Assert.Equal(3, actual);
     }
 }
