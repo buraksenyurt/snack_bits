25d4cf2c3e40a9c9064405535757d21c787e424d
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2023-12-25 08:20:42 +0000

    Initiated a new sample project.

---
+++ 10_plug_in_programming/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _10_plug_in_programming;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 10_plug_in_programming_sdk/Contracts.cs
@@ -0,0 +1,24 @@
+﻿namespace Sdk;
+
+public interface IEffectType
+{
+    string Title { get; set; }
+    string Description { get; set; }
+}
+
+public interface IApplyEffectResponse
+{
+    bool Applied { get; set; }
+    string Error { get; set; }
+    object Result { get; set; }
+}
+
+public interface IApplyEffectRequest
+{
+    object Source { get; set; }
+}
+
+public interface IApplyEffect
+{
+    IApplyEffectResponse Apply(IApplyEffectRequest request);
+}
