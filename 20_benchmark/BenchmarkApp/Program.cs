eab8e9db72723d46931a0ad0024179948bd834ef
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-24 18:25:14 +0300

    Initiate a new project about benchmark tests

---
+++ 20_benchmark/BenchmarkApp/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+BenchmarkDotNet kütüphanesinin kullanıldığı bir örnek uygulamadır.
+Senaryo olarak JSON serileştirme işlemlerinde System.Text.Json
+ve Newtonsoft.Json kütüphanelerinin performansını karşılaştırır.
+ */
+Console.WriteLine("Hello, World!");
+
